30163fb92f33f2197d9229d1b5c604b92e479320
Mattyu <ememattyu12@gmail.com>
2021-06-18 12:14:13 -0600

    Reset

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+const Body = document.querySelector('body');
+const BotonDark = document.querySelector('.btn');
+
+BotonDark.onclick = () => {
+    Body.classList.toggle('dark');
+
+    if (Body.classList.contains('dark')) {
+        localStorage.setItem('ModoDarkAgregado', 'true');
+    } else {
+        localStorage.setItem('ModoDarkAgregado', 'false');
+    }
+}
+
+if (localStorage.getItem('ModoDarkAgregado') === 'true') {
+    Body.classList.add('dark');
+} else {
+    Body.classList.remove('dark');
+}
+
+
 let querty = document.querySelector('.qwerty');
 let search = document.querySelector('.buscar');
 
